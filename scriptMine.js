11261b59b9a7c6de58110a6389899781e87a3a07
christianSplker <103966076+christianSplker@users.noreply.github.com>
2022-12-29 11:16:18 -0700

    Update scriptMine.js

--- scriptMine.js
+++ scriptMine.js
@@ -64,7 +64,7 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
 
 document.querySelector(`.again`).addEventListener(`click`, function () {
   score = 20;
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  secretNumber = Math.trunc(Math.random() * 50) + 1;
   document.querySelector(`.score`).textContent = score;
   document.querySelector(`.number`).textContent = `?`;
   document.querySelector(`body`).style.backgroundColor = `#222`;
